57df97bb6f71084a4d66a239292d0d1027db55e8
Matheus Barbosa <maatheusb96@gmail.com>
2025-08-18 16:53:05 -0300

    Update DiffJParser.groovy

--- src/main/services/dataCollectors/modifiedLinesCollector/DiffJParser.groovy
+++ src/main/services/dataCollectors/modifiedLinesCollector/DiffJParser.groovy
@@ -15,8 +15,8 @@ class DiffJParser {
     private final String REMOVED_FLAG = "d";
 
     private final String FLAGS_REGEX = "(${ADDED_FLAG}|${CHANGED_FLAG}|${REMOVED_FLAG})"
-    private final Pattern CODE_CHANGE_HEADER_REGEX = Pattern.compile("^.* code (added|changed|removed) in .*")
-    private final Pattern METHOD_CHANGE_HEADER_REGEX = Pattern.compile("^.* method (added|changed|removed): .*")
+    private final Pattern STATEMENT_CHANGE_HEADER_REGEX = Pattern.compile("^.* code (added|changed|removed) in .*")
+    private final Pattern METHOD_DECLARATION_CHANGE_HEADER_REGEX = Pattern.compile("^.* method (added|changed|removed): .*")
 
     public Map<String, int[]> parse (List<String> lines) {
         def result = new HashMap<String, int[]>();
@@ -79,9 +79,9 @@ class DiffJParser {
     }
 
     private boolean isMethodChangeLine(String line) {
-        Matcher codeMatcher = CODE_CHANGE_HEADER_REGEX.matcher(line);
+        Matcher codeMatcher = STATEMENT_CHANGE_HEADER_REGEX.matcher(line);
 
-        Matcher methodMatcher = METHOD_CHANGE_HEADER_REGEX.matcher(line);
+        Matcher methodMatcher = METHOD_DECLARATION_CHANGE_HEADER_REGEX.matcher(line);
 
         return codeMatcher.find() || methodMatcher.find();
     }
